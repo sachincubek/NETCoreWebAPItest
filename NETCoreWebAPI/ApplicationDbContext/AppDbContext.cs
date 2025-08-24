d6085df3297ec604a851a15b293fa0383933aeb5
sachincubek <sachincubek@gmail.com>
2025-08-24 09:45:19 +0000

    save changes

---
+++ NETCoreWebAPI/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCoreWebAPI.Models;
+
+namespace NETCoreWebAPI.ApplicationDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
